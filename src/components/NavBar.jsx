757d6872ec0920553f6709995642910ed382e78a
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-10-22 23:20:42 +0530

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import {Navbar,Container,Nav, Image} from 'react-bootstrap';
+import { Navbar, Container, Nav, Image } from "react-bootstrap";
 
 function NavBar() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Image src="https://cdn.discordapp.com/attachments/900789323908153437/901098486311825448/Copy_of_Plantify.png" alt="" style={{width:"2.5rem",height:"2.5rem",margin:"0px 15px",borderRadius:"50%"}}/>
+        <Image
+          src="https://cdn.discordapp.com/attachments/900789323908153437/901098486311825448/Copy_of_Plantify.png"
+          alt=""
+          style={{
+            width: "2.5rem",
+            height: "2.5rem",
+            margin: "0px 15px",
+            borderRadius: "50%",
+          }}
+        />
         <Navbar.Brand href="/">Plantify</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
