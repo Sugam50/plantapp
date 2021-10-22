03ed2b24379d106067178861a8d4e0201b28ebb4
“Rajul” <rajulkoshta10@gmail.com>
2021-10-22 20:59:58 +0530

    Update Crousel.jsx

--- src/components/Crousel.jsx
+++ src/components/Crousel.jsx
@@ -5,22 +5,22 @@ function Crousel() {
     <Carousel indicators    ={false}>
       <Carousel.Item interval={500}>
         <img
-          className="d-block w-100"
-          src="https://i.pinimg.com/originals/44/66/86/4466867576e64e1feb336104a5d90d11.jpg"
+          className="d-block w-100 h-1000"
+          src="https://images.unsplash.com/photo-1417024225833-10a6ab08aa51?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1818&q=80"
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <img
           className="d-block w-100"
-          src="https://i.pinimg.com/originals/44/66/86/4466867576e64e1feb336104a5d90d11.jpg"
+          src="https://images.unsplash.com/photo-1451481086097-f6a15d5f8d18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80"
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <img
           className="d-block w-100"
-          src="https://i.pinimg.com/originals/44/66/86/4466867576e64e1feb336104a5d90d11.jpg"
+          src="https://images.unsplash.com/photo-1529156490981-46d72bba66d4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
           alt="Third slide"
         />
       </Carousel.Item>
