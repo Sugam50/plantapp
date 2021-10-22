fbe99f87f28d2d307d9e71b5f30ba14e010a20dd
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-10-22 18:55:34 +0530

    logo and donor page

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import '../Style/Cards.css'
+import React,{useState} from 'react';
+import '../Style/Cards.css';
+import {Button} from 'react-bootstrap';
 
 function Cards() {
+    const [Option, setOption] = useState(false);
+   
     return (
-        <div className='Cards_list'>
+        <>
+        <div className='Cards_list' onClick={()=>setOption(!Option)}>
             <img src="" alt="" />
             <div className="details">
                 <h5>Name</h5>
@@ -17,6 +21,11 @@ function Cards() {
                 50
             </div>
         </div>
+        <div className={Option?"Options ":"Options hide"}>
+        <Button variant="dark">Donate</Button>
+        <Button variant="dark">Show Profile</Button>
+        </div>
+        </>
     )
 }
 
