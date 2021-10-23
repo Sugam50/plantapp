84af7a1906871d5858b086992bfdd39039c8ab56
“Rajul” <rajulkoshta10@gmail.com>
2021-10-23 17:50:14 +0530

    Update Crousel.jsx

--- src/components/Crousel.jsx
+++ src/components/Crousel.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import image1 from '../assests/bg1.svg'
+import image1 from '../assests/bg1.svg';
 import image2 from "../assests/bg2.svg";
-import image3 from "../assests/bg3.svg";
+import image3 from "../assests/bg3.png";
 
 import {Carousel} from 'react-bootstrap';
 function Crousel() {
   return (
     <Carousel indicators={false}>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={2000}>
         <img
           className="d-block w-100"
           src={image1}
           alt="First slide"
         />
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={2000}>
         <img
           className="d-block w-100"
           src={image2}
           alt="Second slide"
         />
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={2000}>
         <img
           className="d-block w-100"
           src={image3}
