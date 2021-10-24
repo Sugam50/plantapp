e621d0b4bb71e067e20f30f53c382e421e31aaea
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-10-24 10:14:28 +0530

    plantor list added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,28 +2,25 @@ import React,{useState} from 'react';
 import '../Style/Cards.css';
 import {Button} from 'react-bootstrap';
 
-function Cards() {
+function Cards({Name,ImageUrl,details,amount}) {
     const [Option, setOption] = useState(false);
    
     return (
         <>
         <div className='Cards_list' onClick={()=>setOption(!Option)}>
-            <img src="" alt="" />
+            <img src={ImageUrl} alt="" />
             <div className="details">
-                <h5>Name</h5>
-                <p>bvbsfdv
-                    hvbivdbv
-                    nvbivhuovhsdv
+                <h5>{Name}</h5>
+                <p>{details}
                 </p>
             </div>
             <div className="amount">
                 Amount <br/>
-                50
+                ${amount}
             </div>
         </div>
         <div className={Option?"Options ":"Options hide"}>
         <Button variant="dark">Donate</Button>
-        <Button variant="dark">Show Profile</Button>
         </div>
         </>
     )
--- src/pages/Donor.jsx
+++ src/pages/Donor.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import Cards from "../components/Cards";
 import "../Style/Donor.css";
 function Donor() {
+  let DonorArr=[
+    {'Name':"Nick Stalin","ImageUrl":"https://cdn.discordapp.com/attachments/900789323908153437/901691123330863104/17.jpg","details":"Plant Name : Banyan tree Plant phase:3",'amount':"4"},
+    {'Name':"Aman Yadav","ImageUrl":"https://cdn.discordapp.com/attachments/900789323908153437/901691126103293962/26.jpg","details":"Plant Name : Tulsi plant Plant phase:2",'amount':"2"},
+    {'Name':"Ranjan Ninad","ImageUrl":"https://cdn.discordapp.com/attachments/900789323908153437/901691128628277268/28.jpg","details":"Plant Name : Neem plant Plant phase:3",'amount':"5"},
+    {'Name':"Sanjana Deo","ImageUrl":"https://cdn.discordapp.com/attachments/900789323908153437/901691610260197386/3.jpg","details":"Plant Name : Peepal Tree Plant phase:4",'amount':"8"},
+    {'Name':"Pratik Vipin","ImageUrl":"https://cdn.discordapp.com/attachments/900789323908153437/901691143752908820/79.jpg","details":"Plant Name : Mahagony tree Plant phase:3",'amount':"4"},
+    {'Name':"Amit Gayathri","ImageUrl":"https://cdn.discordapp.com/attachments/900789323908153437/901691130771554314/42_1.jpg","details":"Plant Name : Eucalyptus tree Plant phase:2",'amount':"2"},
+  ]
   return (
     <div className="Donor">
       <h1> Welcome!! Your Donations can save the planet.</h1>
       <div className="don_list">
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
-          <Cards/>
+        {
+          DonorArr.map((item,index)=><Cards key={index} Name={item.Name} ImageUrl={item.ImageUrl} details={item.details} amount={item.amount} />)
+        }
       </div>
     </div>
   );
